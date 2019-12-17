af03ad3a08c5d7428c478a3ca1d9386c8b298342
Giannis <savvigiannhs@gmail.com>
2019-12-17 21:50:23 +0200

    initial commit

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import * as firebase from "firebase/app";
+import firebaseConfig from '../firebase-credentials.json';
+
+firebase.initializeApp(firebaseConfig);
 
 Vue.config.productionTip = false
 
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,10 @@
 <template>
-  <div>
-    <h1>Hi</h1>
-  </div>
+  <div></div>
 </template>
 
 <script>
 export default {};
 </script>
+
+<style lang="scss" scoped>
+</style>
